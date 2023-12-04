09e90c64fc923f2606c43e85b3fe680abe735287
BagusFary <bagusfaryananta@gmail.com>
2023-12-04 17:16:30 +0700

    Adding Customers

---
+++ app/api/customers/[id]/route.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+
+export async function GET(req: Request, route:{params:{id:string}})
+{
+    const id = route.params.id
+    const prisma = new PrismaClient()
+    const getCustomer = await prisma.customers.findFirst({
+        where:{
+            id:id
+        }
+    })
+
+    await prisma.$disconnect()
+
+    return Response.json(getCustomer)
+}
+
+// export async function POST(req: Request, route:{params:{id:string}})
+// {
+//     const {name, email, phone} = await req.json()
+//     const id = route.params.id
+//     const prisma = new PrismaClient()
+//     const updateCustomer = await prisma.customers.update({
+//         where:{
+//             id:id
+//         },
+//         data:{
+
+//         }
+//     })
+// }
+
+export async function DELETE(req: Request, route:{params:{id:string}})
+{
+    const id = route.params.id
+    const prisma = new PrismaClient()
+    const deleteCustomer = await prisma.customers.delete({
+        where: {
+            id: id
+        }
+    })
+
+    await prisma.$disconnect()
+
+    return Response.json({
+        success:true,
+        message:"Customer Sucessfully Deleted!"
+    })
+}
