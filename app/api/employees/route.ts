7be4bcdcd30b774b8bdfb1273d56e57debef18f6
BagusFary <bagusfaryananta@gmail.com>
2023-12-04 17:16:56 +0700

    Employees update

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -11,10 +11,20 @@ export async function GET()
 
 export async function POST(req: Request)
 {
-    const {name, employee_code} = await req.json()
+    const {name} = await req.json()
     const prisma = new PrismaClient()
+    const client_code = "SK_"
+    const getEmployeeCode = await prisma.employees.findFirst({
+        orderBy: {
+            created_at: "desc"
+        }
+    })
+    const last_employee_code :any[] = getEmployeeCode?.employee_code.toString().split("_")??[]
+    const number: string = last_employee_code.length != 0 ? last_employee_code[1]  : 0
+    const final_employee_code = Number(number)+1
+    const employee_code = client_code + final_employee_code.toString().padStart(4,"0")
     const pin = await bcrypt.hash('12345678', 10)
-    const admin_id = "4b567e1e-0995-459f-bf95-20635160d9e8"
+    const admin_id = "18cf3c3f-fb35-496f-a862-bac3987bb689"
     const createEmployee = await prisma.employees.create({
         data: {
             name: name,
@@ -25,6 +35,8 @@ export async function POST(req: Request)
     })
 
     await prisma.$disconnect()
+
+    
 
     if(!createEmployee) return Response.json({
         message: "Create Employee Failed"
