1ebb7c1a0bd33d3146863534b2a7dc591bb6205f
BagusFary <bagusfaryananta@gmail.com>
2023-12-13 11:35:55 +0700

    Receipt on progress

--- app/employees/dashboard/(group)/saleshistory/page.tsx
+++ app/employees/dashboard/(group)/saleshistory/page.tsx
@@ -25,13 +25,13 @@ export default function AdminSalesHistoryPage() {
             <input type="text" className='outline outline-1 outline-posgray px-3 rounded-md w-[200px] h-[35px]' placeholder='Search Member' />
           </div> */}
           <div className='mt-3'>
-          <PosTable fixed headers={['Nomor Referensi', 'Total', 'Tanggal', 'Customer ID', 'Employee ID', "Action"]}>
+          <PosTable fixed headers={['Nomor Referensi',  'Tanggal', 'Customer ID', 'Employee ID','Total', "Action"]}>
               <tr>
                 <td className='p-3'>758647857633</td>
-                <td className='p-3'>Rp. 120.000</td>
                 <td className='p-3'>09/12/2023 09.21</td>
                 <td className='p-3'>SK_0004</td>
                 <td className='p-3'>SK1_0054</td>
+                <td className='p-3'>Rp. 120.000</td>
                 <td className='p-3'>
                   <div className='flex gap-2'>
                     <button onClick={() => {}} className='text-black bg-teal-300 px-5 py-1 rounded-md hover:text-white hover:bg-teal-500 transition' ><FontAwesomeIcon icon={faEye}/></button>
@@ -40,10 +40,10 @@ export default function AdminSalesHistoryPage() {
               </tr>
               <tr>
                 <td className='p-3'>758647857633</td>
-                <td className='p-3'>Rp. 120.000</td>
                 <td className='p-3'>09/12/2023 09.21</td>
                 <td className='p-3'>SK_0004</td>
                 <td className='p-3'>SK1_0054</td>
+                <td className='p-3'>Rp. 120.000</td>
                 <td className='p-3'>
                   <div className='flex gap-2'>
                     <button onClick={() => {}} className='text-black bg-teal-300 px-5 py-1 rounded-md hover:text-white hover:bg-teal-500 transition' ><FontAwesomeIcon icon={faEye}/></button>
@@ -53,13 +53,13 @@ export default function AdminSalesHistoryPage() {
           </PosTable>
           </div> 
         </div>
-        <div className='wrapper p-5 shadow-lg w-[450px] h-full bg-white rounded-xl'>
+        <div className='wrapper p-2 shadow-lg w-[450px] h-full bg-white rounded-xl'>
           <div className='flex justify-center items-center'>
             <div className='flex flex-col'>
               <h1 className='text-2xl font-bold flex justify-center'>Transaction Details</h1>
                 <div className='flex flex-col gap-2'>
                 <div className="border-t-[2px] border-solid border-black ..."></div>
-                  <div className='wrapper w-[350px] h-[450px] bg-gray-200'>
+                  <div className='wrapper w-[400px] h-[450px] bg-gray-200'>
                     <div className='flex flex-col'>
                       <div className='flex justify-center mt-5'>
                         <div className='flex gap-1'>
@@ -77,7 +77,36 @@ export default function AdminSalesHistoryPage() {
                         </div>
                       </div>
                       <div className='p-5'>
-                        <div className='border-t-[2px] border-dotted border-black'></div>
+                      <table className='w-full table-auto'>
+                        <thead className='border border-x-0 border-y-2 border-dashed border-black'>
+                          <tr>
+                            <th className='text-start text-xs py-2 w-1/2'>Produk</th>
+                            <th className='text-center text-xs'>Qty</th>
+                            <th className='text-center text-xs'>Harga</th>
+                            <th className='text-center text-xs'>Total</th>
+                          </tr>
+                        </thead>
+                        <tbody className='border border-x-0 border-y-2 border-dashed border-black'>
+                          <tr >
+                            <td className='text-xs font-semibold'>Minyak Sanco 1 Liter </td>
+                            <td className='text-center text-xs font-semibold'>1</td>
+                            <td className='text-center text-xs font-semibold'>10.000</td>
+                            <td className='text-center text-xs font-semibold'>10.000</td>
+                          </tr>
+                          <tr>
+                            <td className='text-xs font-semibold'>Indomie Goreng Mie Rendang Lusin </td>
+                            <td className='text-center text-xs font-semibold'>1</td>
+                            <td className='text-center text-xs font-semibold'>48.000</td>
+                            <td className='text-center text-xs font-semibold'>48.000</td>
+                          </tr>
+                          <tr>
+                            <td className='text-xs font-semibold'>Paseo Tissue Kering </td>
+                            <td className='text-center text-xs font-semibold'>2</td>
+                            <td className='text-center text-xs font-semibold'>12.000</td>
+                            <td className='text-center text-xs font-semibold'>24.000</td>
+                          </tr>
+                        </tbody>
+                      </table>
                       </div>
                     </div>
                   </div>
