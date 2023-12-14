c60d652326f5c8597e135d3c31fe1c7e37067c3b
BagusFary <bagusfaryananta@gmail.com>
2023-12-14 13:23:33 +0700

    Unit Delete Warning

--- app/superadmin/dashboard/(group)/units/page.tsx
+++ app/superadmin/dashboard/(group)/units/page.tsx
@@ -4,9 +4,9 @@ import LoadingComponent from "@/app/_components/LoadingComponent";
 import PosButton from "@/app/_components/PosButton";
 import PosTable from "@/app/_components/PosTable";
 import { fetcher } from "@/app/_lib/Fetcher";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
 import { useRouter } from "next/navigation";
+import Swal from "sweetalert2";
 import useSWR from "swr";
 
 
@@ -14,6 +14,19 @@ export default function SuperAdminUnitsPage()
 {
     const router = useRouter()
     const {data, mutate} = useSWR("/api/superadmin/units",fetcher)
+
+
+    const handleWarning = async (id:any, name:string) => {
+      Swal.fire({
+        title:"Are You Sure?",
+        text:`Are You Sure Want To Delete ${name}?`,
+        icon:"warning",
+        showCancelButton:true,
+        showConfirmButton:true
+      }).then((res) => {
+        if(res.isConfirmed) return handleDelete(id)
+      })
+    }
 
     const handleDelete = async (id:any) => {
       const res = await fetch('/api/superadmin/units/' + id,{
@@ -43,7 +56,7 @@ export default function SuperAdminUnitsPage()
           <td className="p-3">{i+1}</td>
           <td className="p-3">{data.name}</td>
           <td>
-            <button onClick={() => handleDelete(data.id)} className="font-medium text-red-600 dark:text-red-500 hover:underline pr-3">
+            <button onClick={() => handleWarning(data.id, data.name)} className="font-medium text-red-600 dark:text-red-500 hover:underline pr-3">
               Delete
             </button>
             <button onClick={() => router.push('/superadmin/dashboard/units/'+ data.id + '/edit')} className="font-medium text-yellow-800 dark:text-yellow-600 hover:underline">
