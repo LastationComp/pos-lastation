0dc1b24d34546b1325b245936c88f1f26d79a96c
NaafiulRazzaqw <naafiulrazzaq@gmail.com>
2024-01-08 19:03:35 +0700

    Updating Table with new Table Design

--- app/employees/dashboard/(group)/member/page.tsx
+++ app/employees/dashboard/(group)/member/page.tsx
@@ -2,6 +2,7 @@
 import PosButton from '@/app/_components/PosButton'
 import PosTable from '@/app/_components/PosTable'
 import { fetcher } from '@/app/_lib/Fetcher'
+import PosTableNew from '@/app/_lib/NextUiPos/PosTable'
 import { faPencil } from '@fortawesome/free-solid-svg-icons/faPencil'
 import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
 import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash'
@@ -49,6 +50,28 @@ export default function EmployeeMemberPage() {
       icon: "success"
     });
   }
+
+  const memberData = () => {
+    let newData: any[] = [];
+    data?.members.map((member: any, i: number) => {
+      newData.push({
+        key: i + 1,
+        no: i + 1,
+        customer_code: member.customer_code,
+        name: member.name,
+        email: member.email,
+        phone: member.phone,
+        point: member.point,
+        action: <div className='flex gap-2'>
+        <button onClick={() => handleWarning(member.name, member.customer_code)} className='text-white bg-red-500 w-[30px] h-[30px] rounded-md' ><FontAwesomeIcon icon={faTrash}/></button>
+        <button onClick={() => router.push(`/employees/dashboard/member/${member.customer_code}/edit`)} className='text-white bg-yellow-500 w-[30px] h-[30px] rounded-md'><FontAwesomeIcon icon={faPencil}/></button>
+      </div>
+      });
+    });
+
+    return newData;
+
+  }
   
 
   return (
@@ -66,23 +89,7 @@ export default function EmployeeMemberPage() {
           <input type="text" className='outline outline-1 outline-posgray px-3 rounded-md w-[200px] h-[35px]' placeholder='Search Member' />
         </div> */}
         <div className='mt-3'>
-        <PosTable fixed headers={['Customer Code', 'Name', 'Email', 'Phone', 'Point', "Action"]}>
-          {data && data.members.map((member : any, index:number) => (
-            <tr key={index+1}>
-              <td className='p-3'>{member.customer_code}</td>
-              <td className='p-3'>{member.name}</td>
-              <td className='p-3'>{member.email}</td>
-              <td className='p-3'>{member.phone}</td>
-              <td className='p-3'>{member.point}</td>
-              <td className='p-3'>
-                <div className='flex gap-2'>
-                  <button onClick={() => handleWarning(member.name, member.customer_code)} className='text-white bg-red-500 w-[30px] h-[30px] rounded-md' ><FontAwesomeIcon icon={faTrash}/></button>
-                  <button onClick={() => router.push(`/employees/dashboard/member/${member.customer_code}/edit`)} className='text-white bg-yellow-500 w-[30px] h-[30px] rounded-md'><FontAwesomeIcon icon={faPencil}/></button>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </PosTable>
+        <PosTableNew columns={['Customer Code', 'Name', 'Email', 'Phone', 'Point', 'Action']} data={memberData()}/>
         </div> 
       </div>
     </div>
