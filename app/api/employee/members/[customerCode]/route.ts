16e53925b7c4bf8f4b7f2ad7b9f295e8b27c63da
Affansyah Hanan W <afanhanan10@gmail.com>
2024-01-08 11:21:37 +0700

    Fix Some Hook for Deployment:9

--- app/api/employee/members/[customerCode]/route.ts
+++ app/api/employee/members/[customerCode]/route.ts
@@ -1,59 +1,55 @@
-import { responseError, responseSuccess } from "@/app/_lib/PosResponse";
-import { PrismaClient } from "@prisma/client";
+import { responseError, responseSuccess } from '@/app/_lib/PosResponse';
+import { PrismaClient } from '@prisma/client';
 
-export async function GET(req:Request, route:{params:{customerCode:string}})
-{
-    const prisma = new PrismaClient()
-    const customer_code = route.params.customerCode
-    const getMember = await prisma.customers.findFirst({
-        where:{
-            customer_code:customer_code
-        }
-    })
+export async function GET(req: Request, route: { params: { customerCode: string } }) {
+  const prisma = new PrismaClient();
+  const customer_code = route.params.customerCode;
+  const getMember = await prisma.customers.findFirst({
+    where: {
+      customer_code: customer_code,
+    },
+  });
 
-    await prisma.$disconnect()
-    if(!getMember) return false
-    return Response.json({
-        member:getMember
-    })
+  await prisma.$disconnect();
+  if (!getMember) return responseError('Data Not Found');
+  return Response.json({
+    member: getMember,
+  });
 }
 
+export async function POST(req: Request, route: { params: { customerCode: string } }) {
+  const { name, email, phone, point } = await req.json();
+  const prisma = new PrismaClient();
+  const customer_code = route.params.customerCode;
+  const updateMembers = await prisma.customers.update({
+    where: {
+      customer_code: customer_code,
+    },
+    data: {
+      name: name,
+      email: email,
+      phone: phone,
+      point: point,
+    },
+  });
 
-export async function POST(req: Request, route:{params:{customerCode:string}})
-{
-    const {name, email, phone, point} = await req.json()
-    const prisma = new PrismaClient()
-    const customer_code = route.params.customerCode
-    const  updateMembers = await prisma.customers.update({
-        where:{
-            customer_code:customer_code
-        },
-        data:{
-            name:name,
-            email:email,
-            phone:phone,
-            point:point
-        }
-    })
+  await prisma.$disconnect();
 
-    await prisma.$disconnect()
+  if (!updateMembers) return responseError('Failed to Update Member');
 
-    if(!updateMembers) return responseError("Failed to Update Member")
-
-    return responseSuccess("Members Successfully Deleted!")
+  return responseSuccess('Members Successfully Deleted!');
 }
 
-export async function DELETE(req: Request, route:{params:{customerCode:string}})
-{
-    const prisma = new PrismaClient()
-    const customer_code = route.params.customerCode
-    const deleteMembers = await prisma.customers.delete({
-        where:{
-            customer_code:customer_code
-        }
-    })
+export async function DELETE(req: Request, route: { params: { customerCode: string } }) {
+  const prisma = new PrismaClient();
+  const customer_code = route.params.customerCode;
+  const deleteMembers = await prisma.customers.delete({
+    where: {
+      customer_code: customer_code,
+    },
+  });
 
-    await prisma.$disconnect()
-    if(!deleteMembers) return responseError("Failed to Delete Member")
-    return responseSuccess("Member Successfully Deleted!")
+  await prisma.$disconnect();
+  if (!deleteMembers) return responseError('Failed to Delete Member');
+  return responseSuccess('Member Successfully Deleted!');
 }
